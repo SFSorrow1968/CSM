3671c6e7baf4c0a20219d0b7088ce494dcb12da9
SFSorrow1968 <dkatz716@gmail.com>
2026-01-29 19:48:56 -0500

    Move LastEnemy priority to 15 (after BasicKill) so critical/decap/dismember take precedence

--- Configuration/TriggerType.cs
+++ Configuration/TriggerType.cs
@@ -3,11 +3,11 @@ namespace CSM.Configuration
     public enum TriggerType
     {
         BasicKill = 10,
+        LastEnemy = 15,
         Dismemberment = 20,
         Critical = 30,
         Parry = 40,
         Decapitation = 50,
-        LastEnemy = 60,
         LastStand = 100
     }
 }
