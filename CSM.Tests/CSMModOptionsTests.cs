5447802d96f8e803d5edb0d56d0247b1eb313007
SFSorrow1968 <dkatz716@gmail.com>
2026-02-03 19:45:49 -0500

    test(CSM): add ModOptions unit tests
    - Test default values for EnableMod, DebugLogging, DebugOverlay
    - Verify VERSION format
    - Check option and category constants are unique

---
+++ CSM.Tests/CSMModOptionsTests.cs
@@ -0,0 +1,49 @@
+using CSM.Configuration;
+using NUnit.Framework;
+
+namespace CSM.Tests
+{
+    [TestFixture]
+    public class CSMModOptionsTests
+    {
+        [Test]
+        public void ModOptions_DefaultsAreSensible()
+        {
+            // Verify reasonable defaults
+            Assert.That(CSMModOptions.EnableMod, Is.True, "Mod should be enabled by default");
+            Assert.That(CSMModOptions.DebugLogging, Is.False, "Debug logging should be off by default");
+            Assert.That(CSMModOptions.DebugOverlay, Is.False, "Debug overlay should be off by default");
+        }
+
+        [Test]
+        public void ModOptions_VersionIsSet()
+        {
+            // Version should be a valid semver-like string
+            Assert.That(CSMModOptions.VERSION, Is.Not.Null.And.Not.Empty);
+            Assert.That(CSMModOptions.VERSION, Does.Match(@"^\d+\.\d+\.\d+"));
+        }
+
+        [Test]
+        public void ModOptions_OptionConstantsAreDefined()
+        {
+            // Verify option constants exist and are unique
+            var constants = new[]
+            {
+                CSMModOptions.OptionEnableMod,
+                CSMModOptions.OptionDebugLogging,
+                CSMModOptions.OptionDebugOverlay
+            };
+
+            var uniqueSet = new System.Collections.Generic.HashSet<string>(constants);
+            Assert.That(uniqueSet.Count, Is.EqualTo(constants.Length), "Option constants should be unique");
+        }
+
+        [Test]
+        public void ModOptions_CategoryConstantsAreDefined()
+        {
+            // Verify category constants exist
+            Assert.That(CSMModOptions.CategoryPresetSelection, Is.Not.Null.And.Not.Empty);
+            Assert.That(CSMModOptions.CategoryAdvanced, Is.Not.Null.And.Not.Empty);
+        }
+    }
+}
